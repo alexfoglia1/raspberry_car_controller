e1cac77414d49e7ac6bcff4c6a7319f2b218af1b
Alex Foglia <alex.f1793@gmail.com>
2020-11-16 15:45:08 +0100

    first commit

---
+++ src/include/defs.h
@@ -0,0 +1,6 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define PROJNAME "Raspberry PI 4 Car Remote Controller v01.00"
+
+#endif //DEFS_H
---
+++ src/main/main.c
@@ -0,0 +1,12 @@
+#include "defs.h"
+#include "data_transceiver.h"
+#include "video_receiver.h"
+#include "video_renderer.h"
+
+#include <stdio.h>
+
+
+int main(int argc, char** argv)
+{
+    printf("%s\n", PROJNAME);
+}
